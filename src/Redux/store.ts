6177a87691a37bede58ac13640ea883dc3e7d007
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-22 09:54:28 -0300

    criando a logica do Store.ts

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import themeReducer from "./Reducers/themeReducer";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    theme:themeReducer
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
